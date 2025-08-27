5c5b1c7cb8e226cc9f81c1796f395c4e1c14637a
Kamran Shaikh <kamran.aiml.engineer@gmail.com>
2025-08-27 18:07:34 +0530

    Clean up repository: ignore virtual env, update requirements.txt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
@@ -74,11 +73,6 @@ if uploaded_file:
     # Split text
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=400)
     docs = text_splitter.split_documents(data)
-
-    # Create embeddings + vectorstore
-    # embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    # vectorstore = Chroma.from_documents(docs, embedding=embeddings)
-    # retriever = vectorstore.as_retriever(search_type="similarity",search_kwargs={"k": 8} )
 
     # Create embeddings + FAISS vectorstore
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
